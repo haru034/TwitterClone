c1fd2a4120de15e38c18ff74c649720d9b278e42
haru034 <haru034@icloud.com>
2022-02-01 18:53:26 +0900

    DB_HOSTの修正

--- config.php
+++ config.php
@@ -6,7 +6,8 @@ date_default_timezone_set('Asia/Tokyo');
 //URL/ディレクトリ設定
 define('HOME_URL', '/TwitterClone01/');
 //データベースの接続情報
-define('DB_HOST', 'mysql57.haru034.sakura.ne.jp');
+// define('DB_HOST', 'mysql57.haru034.sakura.ne.jp');
+define('DB_HOST', 'mysql1026.db.sakura.ne.jp');
 define('DB_USER', 'haru034');
 define('DB_PASSWORD', 'Kz2bUarET5Xf');
 define('DB_NAME', 'haru034_twitter_clone');
