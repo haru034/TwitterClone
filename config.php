49fbd0cdf204cf0374e45e9bf0e2b2173665efbb
haru034 <haru034@icloud.com>
2022-02-01 16:38:48 +0900

    DB_PASSWORDの修正

--- config.php
+++ config.php
@@ -8,5 +8,5 @@ define('HOME_URL', '/TwitterClone01/');
 //データベースの接続情報
 define('DB_HOST', 'mysql57.haru034.sakura.ne.jp');
 define('DB_USER', 'haru034');
-define('DB_PASSWORD', 'X6hWnVdf');
+define('DB_PASSWORD', 'Kz2bUarET5Xf');
 define('DB_NAME', 'haru034_twitter_clone');
